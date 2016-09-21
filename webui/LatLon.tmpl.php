84f36f9fc0327e76640032c5544f681d76826064
crescentvenus <ei2k-hrai@asahi-net.or.jp>
2016-09-21 16:59:33 +0900

    Update LatLon.tmpl.php

--- webui/LatLon.tmpl.php
+++ webui/LatLon.tmpl.php
@@ -8,30 +8,48 @@
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Cache-Control" content="no-cache">
     <meta http-equiv="Expires" content="<?= gmdate('D, d M Y H:i:s').' GMT' ?>">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="LatLon.css" rel="stylesheet">
 </head>
 <body>
-    <div id="body" class="container">
-        <table id="settings" class="fluid">
-            <tr><td class="align-left">
-                <form id="loc" method="post" accept-charset="utf-8">
-                    <label class="text-bold"> Location: </label>
-                    <input type="hidden" id="zoom" name="zoom" value="<?php echo $settings['zoom'] ?>">
-                    <input id="lat" type="text" name="latitude" class="form-control text-red text-bold" placeholder="Latitude..." value="<?php echo $settings['latitude']?>"/>
-                    <input id="lng" type="text" name="longitude" class="form-control text-red text-bold" placeholder="Longitude..." value="<?php echo $settings['longitude']?>"/>
-                    <!--
-                    <input id="sub" type="button" value="Submit" class="form-control form-button text-bold"
-                    onClick="sendLocation('http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']?>','#loc');return false;">
-                    -->
-                </form>
-            </td><td class="align-right">
-                <form id="SIM" method="POST" action="LatLon.php">
-                    <input id="start" type="submit" value="SimStart" name="start" class="form-control form-button text-red text-bold">
-                    <input id="stop" type="submit" value="SimStop" name="stop" class="form-control form-button text-red text-bold">
-                </form>
-            </td></tr>
-        <table>
-        <div id="map" style="width:200px;height:200px;"></div>
+    <div  id="body" class="container-fluid">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <table><tr><td class="form-array">
+                    <div class="form-group form-inline">
+                    <form id="loc" method="post" accept-charset="utf-8">
+                        <label for="text">Location: </label>
+                        <input type="hidden" id="zoom" name="zoom" value="<?php echo $settings['zoom'] ?>">
+                        <input id="lat" type="text" name="latitude" size="8"  placeholder="Latitude..." value="<?php echo $settings['latitude']?>"/>
+                        <input id="lng" type="text" name="longitude" size="8" placeholder="Longitude..." value="<?php echo $settings['longitude']?>"/>
+
+                        <label for="text">Max Speed: </label>
+                                                <select name="speed">
+                                                        <?php $speed=$settings['speed']?>
+                                                        <option value="40"   <?php if($speed==40)   echo selected ?>>5Km</option>
+                                                        <option value="80"   <?php if($speed==80)   echo selected ?>>10Km</option>
+                                                        <option value="400"  <?php if($speed==400)  echo selected ?>>50Km</option>
+                                                        <option value="800"  <?php if($speed==800)  echo selected ?>>100Km</option>
+                                                        <option value="1600" <?php if($speed==1600) echo selected ?>>200Km</option>
+                                                        <option value="2400" <?php if($speed==2400) echo selected ?>>300Km</option>
+                                                </select>
+<!--
+                        <input id="sub" type="button" value="Submit"
+                        onClick="sendLocation('http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']?>','#loc');return false;">
+-->
+                    </form>
+                    </div>
+                </td><td class="form-array">
+                    <div class="form-group form-inline">
+                    <form id="SIM" method="POST" action="LatLon2.php">
+                                        <input id="start" type="submit" value="SimStart" name="start">
+                                        <input id="stop" type="submit" value="SimStop" name="stop">
+                        </form>
+                    </div>
+                </td></tr><table>
+                <div id="map" style="width:1000px;height:700px;"></div>
+            </div>
+        </div>
     </div>
 
     <script type="text/javascript">
@@ -40,6 +58,7 @@
     _googleMapInitSetting =
         {
             zoom: <?php echo $settings['zoom'] ?>,
+                        scaleControl: true,
             center: {
                 lat: <?php echo $settings['latitude'] ?>,
                 lng:<?php echo $settings['longitude'] ?>
@@ -47,6 +66,7 @@
         };
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="./LatLon.js"></script>
     <script src="http://maps.googleapis.com/maps/api/js?signed_in=true&callback=initMap"></script>
 </body>
