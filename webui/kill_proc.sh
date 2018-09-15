a2d453c0fc40f91ddd9b31a373e85f76f071029c
crescentvenus <horiai.keiichi@gmail.com>
2018-09-15 21:50:35 +0900

    Update kill_proc.sh

--- webui/kill_proc.sh
+++ webui/kill_proc.sh
@@ -1,23 +1,11 @@
 #!/bin/sh
-PID=`ps ax | grep smooth.php | grep -v "sh -c " | grep -v grep | awk '{print $1}'`
-if  test -z $PID  ; then
-        echo "No such a process"
-else
-        echo $PID
-        kill -6 $PID
-fi
-PID=`ps ax | grep hackrf | grep -v grep | awk '{print $1}'`
-PID=`ps ax | grep gps-sdr-sim | grep -v "sh -c " | grep -v grep | awk '{print $1}'`
-if  test -z $PID  ; then
-        echo "No such a process"
-else
-        echo $PID
-        kill -6 $PID
-fi
-PID=`ps ax | grep hackrf | grep -v grep | awk '{print $1}'`
-if  test -z $PID  ; then
-        echo "No such a process"
-else
-        echo $PID
-        kill -6 $PID
-fi
+for proc in smooth2.php hackrf  gps-sdr-sim
+do
+        PID=`ps ax | grep $proc | grep -v "sh -c " | grep -v grep | awk '{print $1}'`
+        if  test -z $PID  ; then
+                echo "No such a process"
+        else
+                echo $PID
+                kill -6 $PID
+        fi
+done
